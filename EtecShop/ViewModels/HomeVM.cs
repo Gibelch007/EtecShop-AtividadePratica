d292493aea286920fe5ec9798e22cf46aafff6f7
gibelch007 <giovanaferrodrigues@gmail.com>
2024-06-03 09:00:12 -0300

    projeto dem avaliação em sala de aula professor Gallo

---
+++ EtecShop/ViewModels/HomeVM.cs
@@ -0,0 +1,11 @@
+using EtecShop.Models;
+
+namespace EtecShop.ViewModels;
+
+public class HomeVM
+{
+    public List<Categoria> Categorias {get; set;}
+    public List<Categoria> Produtos {get; set;}
+    
+}
+
