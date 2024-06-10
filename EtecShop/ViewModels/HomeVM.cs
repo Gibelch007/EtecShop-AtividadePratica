66277b6dffdbf101a1d2cb306d40ae0084a2b690
gibelch007 <giovanaferrodrigues@gmail.com>
2024-06-10 08:38:39 -0300

    finalização do projeto avaliativo

--- EtecShop/ViewModels/HomeVM.cs
+++ EtecShop/ViewModels/HomeVM.cs
@@ -1,11 +1,8 @@
 using EtecShop.Models;
-
 namespace EtecShop.ViewModels;
-
 public class HomeVM
 {
-    public List<Categoria> Categorias {get; set;}
-    public List<Categoria> Produtos {get; set;}
-    
+    public List<Categoria> Categorias { get; set; }
+    public List<Produto> Produtos { get; set; }
 }
 
--- EtecShop/ViewModels/ProdutoVM.cs
+++ EtecShop/ViewModels/ProdutoVM.cs
@@ -1,12 +1,10 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-namespace EtecShop.Models;
-[Table("Avaliacao")]
-public class Avaliacao
+using EtecShop.Models;
+namespace EtecShop.ViewModels;
+public class ProdutoVM
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public Produto Produto { get; set; }
+    public List<Avaliacao> Avaliacoes { get; set; }
     [Required(ErrorMessage = "Informe o Nome")]
     [StringLength(60, ErrorMessage = "O Nome deve possuir até 60 caracteres")]
     public string Nome { get; set; }
@@ -16,11 +14,4 @@ public class Avaliacao
     [Required(ErrorMessage = "Escreva sua Avaliação")]
     [StringLength(300, ErrorMessage = "A Avaliação de possuir no máximo 300 caracteres")]
     public string Texto { get; set; }
-    [Display(Name = "Data da Avaliação")]
-    public DateTime DataAvaliacao { get; set; } = DateTime.Now;
-    [Display(Name = "Produto")]
-    [Required(ErrorMessage = "Informe o Produto")]
-    public int ProdutoId { get; set; }
-    [ForeignKey("ProdutoId")]
-    public Produto Produto { get; set; }
 }
